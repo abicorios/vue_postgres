163fbc7c73581c5b097bf0fb74c1581d4a9dd3c1
abicorios <yaroslav.moroz91@gmail.com>
2020-01-28 19:39:02 +0200

    improve stability on Ubuntu server

--- api.php
+++ api.php
@@ -1,4 +1,4 @@
 <?php
-$connect = new PDO('pgsql:dbname=test;user=test;password=test');
+$connect = new PDO('pgsql:host=localhost;port=5432;dbname=test;user=test;password=test');
 $result = $connect->query('select message from messages where id = 1;')->fetch(PDO::FETCH_ASSOC);
 echo json_encode($result);
